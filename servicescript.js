2eaf3c9c08999e0b711a322a4c80dd5ee2c5c602
YanisHL <166521908+YanisHL@users.noreply.github.com>
2024-05-02 16:56:00 +0200

    fin

--- servicescript.js
+++ servicescript.js
@@ -12,11 +12,47 @@ fetch("restaurantBresilien.json")
 function addProducts(data) {
   let serv = "";
   data.entreprise.services.forEach((s) => {
-    serv += `<div class="cardservice"><h3>${s.nom}</h3> 
-              <p>${s.description}</p> 
-              <video src="${s.image}" alt=""> </video>
+    serv += `<div class="cardservice flex spacebetween">
+    <h3 class="h3serv">${s.nom}</h3> 
+    <div class="nomdesc">
+              <p class="pserv">${s.description}</p> 
+              
+              <video autoplay muted loop class="videoserv" src="${s.image}" alt=""> </video>
+              </div>
               <div>`;
   });
 
   document.querySelector(".contentservice").innerHTML += `${serv}`;
 }
+
+AOS.init();
+// CARTES
+var map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+let coord = [43.3, 5.4];
+var redIcon = L.icon({
+  iconUrl: "/assets/leaf-red.png",
+  shadowUrl: "/assets/leaf-shadow.png",
+
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
+var marquer = L.marker(coord, { icon: redIcon })
+  .addTo(map)
+  .bindPopup("você está aqui !")
+  .openPopup();
+var circle = L.circle([43.3, 5.4], {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 0.5,
+  radius: 1000,
+}).addTo(map);
+var popup = L.popup();
